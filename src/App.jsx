be938e4c7c6da65bafbf28109f642a005bb3802e
Marian <hellomariandev@gmail.com>
2022-08-08 20:13:03 +0200

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Header from "./components/Header";
+import SignUp from "./components/SignUp";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>React Form</h1>
-      <h2>Sign Up with Formik</h2>
+      <Header />
+      <SignUp />
     </div>
   );
 }
