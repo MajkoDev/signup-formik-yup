0679c6093e18a08fb206b75f087c17979eb29e11
Marian <hellomariandev@gmail.com>
2022-08-08 20:13:11 +0200

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import './header.css'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>React Form</h1>
+      <h2>Sign Up with Formik</h2>
+    </div>
+  );
+}
