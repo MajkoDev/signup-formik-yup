1d24d488c62d2552f4afc424169c98382a88b27a
Marian <hellomariandev@gmail.com>
2022-08-08 19:39:25 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Form</h1>
+      <h2>Sign Up with Formik</h2>
+    </div>
+  );
+}
+
+export default App;
